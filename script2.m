47975abfe39b31ecd51c5c1299c1abc588e46793
Lachrynier <97088473+Lachrynier@users.noreply.github.com>
2023-02-14 09:31:30 +0100

    Add files via upload

--- script2.m
+++ script2.m
@@ -3,42 +3,18 @@ clear, close all
 %%
 clear
 %%
-I = zeros(256,256,64);
-for k = 1:64
-    filename = sprintf('toyProblem_F22/frame_%02d.png',k);
-    I(:,:,k) = im2double(rgb2gray(imread(filename)));
+I = zeros(256,144,82-40+1);
+A = imresize( im2double(rgb2gray(imread('video/00005.png'))), 1/5 );
+for k = 40:82
+    I(:,:,k) = imresize( im2double(rgb2gray(imread(sprintf('video/%05d.png',k)))), 1/5 );
 end
 
 figure(1)
 sliceViewer(I);
 
-
-Vx = I(2:end,:,:) - I(1:end-1,:,:);
-Vy = I(:,2:end,:) - I(:,1:end-1,:);
-Vt = I(:,:,2:end) - I(:,:,1:end-1);
-
 %%
-figure(2)
-sliceViewer(im2double(abs(Vx)));
-figure(7)
-sliceViewer(im2double(abs(Vy)));
-%%
-PWx = [1,0,-1; 1,0,-1; 1,0,-1];
-PWy = [1,1,1; 0,0,0; -1,-1,-1];
-I_PWx = imfilter(I,PWx);
-I_PWy = imfilter(I,PWy);
-
-figure(3)
-sliceViewer(im2double(abs(I_PWx)));
-figure(4)
-sliceViewer(im2double(abs(I_PWy)));
-
-%%
-% d/dx(G(x)*G(y)) = G'(x)*G(y) + G(x)*G'(y)
-% n 5, 20
-% s 1, 10
-n = 20;
-s = 10;
+n = 5;
+s = 1;
 m = (n+1)/2;
 
 G = @(x,m,s) 1/(s*sqrt(2*pi)) .* exp(-(x-m).^2 ./ (2*s^2));
@@ -70,7 +46,7 @@ I_filterT = imfilter(I_filterT,permute(g_filter,[2,1,3]),'replicate');
 I_filterT = imfilter(I_filterT,permute(dg_filter,[1,3,2]),'replicate');
 
 %%
-for t = 10:55
+for t = 45:77
     N = 9;
     A = zeros(N^2,2);
     b = zeros(N^2,1);
@@ -79,7 +55,7 @@ for t = 10:55
     % x = 5:10:250;
     % y = 5:10:250;
     x = 10:5:240;
-    y = 10:5:240;
+    y = 10:5:135;
     count = 1;
     for i = x
         for j = y
@@ -90,7 +66,7 @@ for t = 10:55
             A(:,2) = reshape(I_filterY(idx_x,idx_y,t),[N^2,1]);
             b(:) = reshape(I_filterT(idx_x,idx_y,t),[N^2,1]);
             tmp = A\(-b);
-            if norm(tmp) < 1
+            if norm(tmp) < 0.2
                 continue
             end
             dX(count) = tmp(1);
@@ -110,72 +86,13 @@ for t = 10:55
     H = gcf;
     H.WindowState = 'maximized';
 %     exportgraphics(gcf,sprintf('visualization/frame_%02d.png',t),'Resolution',1000)
-    saveas(gcf, sprintf('visualization/frame_%02d.png',t));
+    saveas(gcf, sprintf('video_analysis/frame_%02d.png',t));
 end
 
 %%
 
-for k = 10:55
-    filename = sprintf('visualization/frame_%02d.png',k);
+for k = 45:77
+    filename = sprintf('video_analysis/frame_%02d.png',k);
     vis(:,:,k) = im2double(rgb2gray(imread(filename)));
 end
 sliceViewer(vis);
-
-%%
-N = 9;
-A = zeros(N^2,2);
-b = zeros(N^2,1);
-%[x,y,t] = deal(130,100,32);
-t = 56;
-% x = 5:10:250;
-% y = 5:10:250;
-x = 10:5:240;
-y = 10:5:240;
-count = 1;
-for i = x
-    for j = y
-        
-        idx_x = (1:N)-(N+1)/2 + i;
-        idx_y = (1:N)-(N+1)/2 + j;
-        A(:,1) = reshape(I_filterX(idx_x,idx_y,t),[N^2,1]);
-        A(:,2) = reshape(I_filterY(idx_x,idx_y,t),[N^2,1]);
-        b(:) = reshape(I_filterT(idx_x,idx_y,t),[N^2,1]);
-        tmp = A\(-b);
-        if norm(tmp) < 1
-            continue
-        end
-        dX(count) = tmp(1);
-        dY(count) = tmp(2);
-        X(count) = i;
-        Y(count) = j;
-        count = count + 1;
-    end
-end
-
-figure(2)
-subplot(2,2,4)
-imshow(I(:,:,t))
-hold on
-alpha = 1;
-quiver(Y,X,alpha.*dX,alpha.*dY,2,LineWidth=2)
-
-%%
-figure(5)
-H = gcf;
-H.WindowState = 'maximized';
-
-sliceViewer(abs(I_filterX));
-figure(6)
-sliceViewer(abs(I_filterY));
-figure(8)
-sliceViewer(abs(I_filterT));
-
-%%
-% hzm = zeros(1,1,3);
-% hzm(1,1,:) = [1,0,-1];
-% hy = zeros(3,1,1);
-% hy(:,1,1) = [1,2,1];
-% hx = zeros(1,3,1);
-% hx(1,:,1) = [1,2,1];
-% 
-% hx'*hy
